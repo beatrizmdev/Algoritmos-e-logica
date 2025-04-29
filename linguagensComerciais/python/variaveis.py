eb47fcb4de09beb2f2b2fa5d0afbebad411638ba
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-04-28 21:51:21 -0300

    feat: interpolating variables

--- linguagensComerciais/python/variaveis.py
+++ linguagensComerciais/python/variaveis.py
@@ -2,3 +2,13 @@ descricaoVaga: str
 quantidade: int
 salario: float
 abertaParaIncricoes: bool
+
+#definição de variáveis
+descricaoVaga = "Programador Júnior"
+quantidade = 5
+salario= 3555.78
+abertaParaIncricoes = True
+
+print(f"Descrição da vaga: {descricaoVaga}, salário: R${salario}")
+print(f"Quantidade: {quantidade}")
+print(f"Aberta: {abertaParaIncricoes}")
