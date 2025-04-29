c2136ec2d4741b177de9d5d8dda4c5bbed83d657
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-04-28 21:58:42 -0300

    feat: interpolation and concatenation

---
+++ linguagensComerciais/python/interpolacaoOuConcatenacao.py
@@ -0,0 +1,6 @@
+nome = "Ana"
+idade = 5
+
+print(nome + " é feia") #concatenação
+print(f"{nome} é feia e tem {idade} anos.") #interpolação
+print(nome, " é feia e tem ", idade, " anos") #o Python entende como argumentos de uma função, mas não é uma concatenação nem interpolação
