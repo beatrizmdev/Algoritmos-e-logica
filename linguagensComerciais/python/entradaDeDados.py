17b0a337c86dda7d788ab9644f6475d32a6f9e09
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-04-30 19:08:09 -0300

    data input with casting

--- linguagensComerciais/python/entradaDeDados.py
+++ linguagensComerciais/python/entradaDeDados.py
@@ -1,11 +1,14 @@
 print("Calculadora de IMC")
 
+# casting (conversão de tipo)
+
 nome = input("Informe o nome: ")
-peso = input("Informe o peso: ")
-altura = input("Informe a altura: ")
+peso = int(input("Informe o peso: "))
+altura = float(input("Informe a altura: "))
 
 #input retorna sempre uma string, mesmo que vc tenha tipado anteriormente.
 
 print(f"Nome: {nome} - Peso: {peso} - Altura: {altura}")
 
 imc = peso / (altura * altura)
+print(f"IMC = {imc:.2f}")
