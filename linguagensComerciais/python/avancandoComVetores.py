100c002e4a5f20fd5826ab3dccc369f9eb9db5c9
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-03 19:21:05 -0300

    feat: advancing with lists

---
+++ linguagensComerciais/python/avancandoComVetores.py
@@ -0,0 +1,28 @@
+"""
+# declarando vetores no formato de lista
+frutas = ["Abacaxi", "Uva", "Banana", 10, 55.33, "Mamão", "Pera"]
+
+print(len(frutas[2]))
+
+# percorrendo vetores de tamanhos dinâmicos
+for i in range(0,len(frutas)):
+    print(frutas[i])
+
+print("-----------------")
+
+# inclusão de elementos em um vetor já declarado (precisa criar um novo "espaço")
+frutas.append("Maçã")
+# percorrendo vetores de tamanhos dinâmicos
+for i in range(0,len(frutas)):
+    print(frutas[i])
+"""
+
+valores = []
+valor = int(input("Digite um valor: "))
+
+while valor >= 0:
+    valores.append(valor)
+    valor = int(input("Digite um valor: "))
+
+for i in range(0, len(valores)):
+    print(valores[i])
