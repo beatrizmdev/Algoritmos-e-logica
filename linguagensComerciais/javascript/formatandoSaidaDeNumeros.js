24fb028eed28222ca3efd0907ff79d71026540cf
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-08 10:41:47 -0300

    feat: formatting number output

---
+++ linguagensComerciais/javascript/formatandoSaidaDeNumeros.js
@@ -0,0 +1,6 @@
+var valorDeCompra = 88.98651
+var valorDeVenda = 114.45487
+var quantidade = 7
+
+console.log(`Valor de compra: ${valorDeCompra} - Valor de venda: ${valorDeVenda} - Quantidade: ${quantidade}`)
+console.log(`Valor de compra: ${valorDeCompra.toFixed(2)} - Valor de venda: ${valorDeVenda.toFixed(2)} - Quantidade: ${quantidade.toFixed(3)}`)
