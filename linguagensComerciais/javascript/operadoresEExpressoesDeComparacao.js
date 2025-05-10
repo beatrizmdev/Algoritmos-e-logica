fd103575eefc38bcd445513ff978e49a2b7c1fc4
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-09 22:52:05 -0300

    feat: case sensitive - comparison operators and expressions

---
+++ linguagensComerciais/javascript/operadoresEExpressoesDeComparacao.js
@@ -0,0 +1,7 @@
+console.log(10 > 5)
+console.log(7 < 3)
+console.log(9 >= 9)
+console.log(18 <= 11)
+console.log("abacaxi" == "uva")
+console.log("Uva" == "uva") // FALSE - javascript é case sensitive
+console.log("bicicleta" != "joao")
