f42b81076b48ea1c216225ec93b3a736fd31294c
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-14 23:18:01 -0300

    feat: comments in C

---
+++ linguagensComerciais/c/C/comentarios.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    // comentário de linha única
+    /*
+        comentário de
+        linhas múltiplas
+    */
+
+    setlocale(LC_ALL, "Portuguese");
+    printf("Nada de comentários\n");
+    return 0;
+}
