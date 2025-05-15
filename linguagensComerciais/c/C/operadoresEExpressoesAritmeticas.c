891d3db18f7a482989be75b823c2612594ef9110
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-15 18:46:34 -0300

    feat: arithmetic operators and expressions

---
+++ linguagensComerciais/c/C/operadoresEExpressoesAritmeticas.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int divisao = 4 / 2;
+
+    printf("soma: %d \n", 7 + 8);
+    printf("soma de reais: %.2lf \n", 4.2 + 2);
+    printf("subtração: %.2lf \n", 4.4 - 4);
+    printf("multiplicação: %i \n", 2 * 4);
+    printf("divisão: %i \n", divisao);
+    printf("divisao que resultaria em real, placeholder int: %i \n", 9 / 2);
+    printf("divisao que resultaria em real, placeholder double: %.2lf \n", 9.0 / 2);
+    printf("mod: %i \n", 9 / 2);
+
+    return 0;
+}
