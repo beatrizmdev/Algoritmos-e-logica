517a9d06dc756b7d9a1cdf99dbad1a2eb237c934
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-17 23:37:01 -0300

    feat: input of special characters in the console by changing the codepage

--- linguagensComerciais/c/C/entradaDeDadosComFgets.c
+++ linguagensComerciais/c/C/entradaDeDadosComFgets.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <locale.h>
 #include <string.h>
+#include <windows.h>
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese"); // trata somente da exibição, não da leitura
+
+    // Valores originais entrada/saída
+    int cpIn = GetConsoleCP(); // Identificador codepage entrada
+    int cpOut = GetConsoleOutputCP(); // Identificador codepage saída
+
+    // Ajustar entrada/saída codepage console
+    SetConsoleCP(1252); // estabelece  codepage entrada
+    SetConsoleOutputCP(1252); // estabelece  codepage saída
+
+    /*
+    cpIn = GetConsoleCP();
+    cpOut = GetConsoleOutputCP();
+
+    printf("E: %d \n", cpIn);
+    printf("S: %d \n", cpOut);
+
+    return 0;
+    */
 
     // declaração de variáveis
     char logradouro[50];
@@ -27,6 +46,10 @@ int main()
     printf("Tipo de imóvel: %c \n", tipoImovel);
     printf("Metragem: %i\n", metrosQuadrados);
     printf("Preço: %.2lf\n", valor);
+
+    // Reajustar valores padrões entrada/saída codepage console
+    SetConsoleCP(850); // estabelece  codepage entrada
+    SetConsoleOutputCP(850); // estabelece  codepage saída
 
     return 0;
 }
