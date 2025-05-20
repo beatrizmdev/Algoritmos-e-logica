58b25310105d6d50eec4c835e32b8994a7056846
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-20 17:59:16 -0300

    fix: while loop structure now working

--- linguagensComerciais/c/C/estruturaWhile.c
+++ linguagensComerciais/c/C/estruturaWhile.c
@@ -30,7 +30,7 @@ int main()
 
         printf("Deseja continuar (s)? ");
         fflush(stdin);
-        scanf("%c", &continuar);
+        scanf(" %c", &continuar);
     }
 
     printf("Valor total: %.2lf", total);
