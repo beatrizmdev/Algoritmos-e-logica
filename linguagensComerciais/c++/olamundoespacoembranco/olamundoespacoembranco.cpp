9bebd618164ac5428e580eb53cc99dd5178c65df
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-28 21:04:00 -0300

    feat: hello world without required indentation

---
+++ linguagensComerciais/c++/olamundoespacoembranco/olamundoespacoembranco.cpp
@@ -0,0 +1,6 @@
+// Em c++ os espaços em brancos e indentação são completamente ignorados,
+// o fazemos só pra tornar a leitura mais fácil.
+
+#include <iostream>
+using namespace std;
+int main() {cout << "Olá Mundo, sem espaços!"; return 0;}
