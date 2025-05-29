3f26f670dc200e5ce25c5fcaed83c8c669ff54b0
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-29 17:04:48 -0300

    feat: data print

---
+++ linguagensComerciais/c++/impressaodedados/impressaodedados.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "saída 1 \n\n";
+    cout << "saída 2" <<endl;
+    cout << "saída 3";
+    // <<endl e "\n" funcionam da mesma forma
+
+    return 0;
+}
