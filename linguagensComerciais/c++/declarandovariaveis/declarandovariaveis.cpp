0ea5e835f62156737f5d02069740cac2a339fb05
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-28 22:04:18 -0300

    feat: declaring multiple variables and the const type

--- linguagensComerciais/c++/declarandovariaveis/declarandovariaveis.cpp
+++ linguagensComerciais/c++/declarandovariaveis/declarandovariaveis.cpp
@@ -3,11 +3,21 @@ using namespace std;
 
 int main() {
 
-    int idade = 30;
+//    int idade = 30;
     string msg;
+//    int minhaIdade = 25;
+//    int somaIdades;
+    int idade = 30, minhaIdade = 25, somaIdades;
+    
+    const int ano = 2025;
+
     msg = " anos.";
     idade = 20;
+    
+    somaIdades = idade + minhaIdade;
 
-    cout << idade;
+    cout << "Olá, minha idade é "<<idade<<msg;
+    cout << "A nossa idade somada é "<<somaIdades;
+    cout << "O ano é "<<ano;
     return 0;
 }
