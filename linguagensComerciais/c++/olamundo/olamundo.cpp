4556aa20909956d13e3c2795c9b363ece6b2e0c3
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-28 21:01:57 -0300

    feat: hello world in c++!

---
+++ linguagensComerciais/c++/olamundo/olamundo.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // biblioteca de cabeçalho que adiciona funcionalidades ao programa
+using namespace std; // significa que podmos usar nomes para objetos e variáveis de bibliotecas padrões
+
+int main() {
+    cout << "Olá Mundo!"; // cout (se pronuncia c-out) é um objeto usado pra impressão de texto e "<<" é o "recebe"
+    return 0;
+    
+}
