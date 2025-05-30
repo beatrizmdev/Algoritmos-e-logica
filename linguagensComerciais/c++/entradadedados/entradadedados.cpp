de61ac13d02e906dde68c63308c3afeec061d411
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-05-29 22:51:37 -0300

    feat: data input

---
+++ linguagensComerciais/c++/entradadedados/entradadedados.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    int soma;
+
+    cout << "primeiro numero: ";
+    cin >> x;
+
+    cout << "segundo numero: ";
+    cin >> y;
+
+    soma = x + y;
+
+    cout << "resultado da soma: "<< soma;
+    
+    return 0;
+}
