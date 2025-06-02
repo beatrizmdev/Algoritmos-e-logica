be57c5a0ed31c4140a6487888559be21a38c47b4
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-01 22:45:06 -0300

    feat: simple conditional structure

---
+++ linguagensComerciais/c++/estruturacondicionalsimples/estruturacondicionalsimples.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int valor = 20;
+    bool isBig = valor > 100;
+
+    if (isBig) {
+        cout << "20 é maior que 100 \n";
+    }
+    
+    return 0;
+}
