27f48e85e7fc35f6cbcc806eb4c95c0fc9c5d32b
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-02 22:24:49 -0300

    feat: while loop

---
+++ linguagensComerciais/c++/while/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero = 1;
+
+    while (numero <= 5) {
+        cout << numero << "º execução do bloco de código\n";
+        
+        numero++;
+    }
+
+    return 0;
+}
