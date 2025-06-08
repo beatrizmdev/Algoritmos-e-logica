09e87e711d829a7313d562437135988005739a87
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-08 14:01:54 -0300

    feat: types of variables

---
+++ linguagensComerciais/c#/tiposDeVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 30;
+            int idadeFilho;
+            int idadeX = 10, idadeY = 20, idadeZ = 30; 
+            const int anoAtual = 2024;
+
+            idadeFilho = 20;
+
+            Console.WriteLine("Olá, minha idade é " + idade + " e a idade do meu filho é " + idadeFilho + " no ano atual " + anoAtual);
+
+        }
+    }
+}
