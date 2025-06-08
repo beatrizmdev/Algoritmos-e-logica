4f4760d74b4c9a224dab4d5859b08215c05fbb92
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-08 14:20:34 -0300

    feat: data types

---
+++ linguagensComerciais/c#/tiposDeDados/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace tiposDeDados
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double salarioA = 3000.50;
+            long salarioB = 300050;
+            bool estaChovendo = false;
+            char estaChovendo2 = 'S';
+            string msg = "Está chovendo? ";
+            var graus = 30;
+            var msg2 = "Graus hoje: ";
+
+            Console.WriteLine(msg + estaChovendo2);
+            Console.WriteLine(msg2 + graus);
+
+        }
+    }
+}
