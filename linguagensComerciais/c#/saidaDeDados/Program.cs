b277f1ad6e187ce38db3bc5e7db36aaf07938c75
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-09 11:51:46 -0300

    feat: data output

---
+++ linguagensComerciais/c#/saidaDeDados/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace saidaDeDados
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("saída 1: ");
+            Console.WriteLine("saída 2: ");
+            Console.Write("saída 3: \n");
+            Console.Write("saída 4: ");
+            Console.Write("saída 5: ");
+        }
+    }
+}
