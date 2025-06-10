24b1b5bdf4803990eb3bdd2cce014ab8ec54fb80
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-10 18:53:53 -0300

    feat: switch case structure

---
+++ linguagensComerciais/c#/switchCase/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace saidaDeDados
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 8;
+
+            switch (idade)
+            {
+                case < 18:
+                    Console.WriteLine("Menor de idade");
+                    break;
+                case < 60:
+                    Console.WriteLine("Maior de idade");
+                    break;
+                case >= 60:
+                    Console.WriteLine("Idoso");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+}
