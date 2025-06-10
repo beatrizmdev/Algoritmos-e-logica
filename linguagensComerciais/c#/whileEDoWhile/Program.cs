0cb6599f9241a9fc446dc26cff1e6b46c310b34b
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-10 18:53:00 -0300

    feat: while and do while loop structure

---
+++ linguagensComerciais/c#/whileEDoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace saidaDeDados
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int num2 = 1;
+
+            while (num <= 5)
+            {
+                Console.WriteLine(num + "º execução do código.");
+                num++;
+            }
+
+            Console.WriteLine("\n------------------------\n");
+
+            do
+            {
+                Console.WriteLine(num2 + "º execução do código.");
+                num2++;
+            } while (num2 <= 5);
+        }
+    }
+}
