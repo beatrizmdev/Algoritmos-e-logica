ea93082bdaaa375e9a8a21816556a7f9e1725888
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-10 18:52:17 -0300

    feat: conditional structure

---
+++ linguagensComerciais/c#/estruturaCondicional/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace estruturaCondicional
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool online = true;
+            bool aniversario = true;
+
+            if (online == true)
+            {
+                Console.WriteLine("Você está conectado.");
+
+                if (aniversario == true)
+                {
+                    Console.WriteLine("Parabéns! Aproveite o seu aniversário!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Você está desconectado.");
+                
+                if (aniversario == true) {
+                    Console.WriteLine("Hoje é o seu aniversário!\nConecte-se aos seus amigos para comemorar seu aniversário.");
+                }
+            }           
+        }
+    }
+}
