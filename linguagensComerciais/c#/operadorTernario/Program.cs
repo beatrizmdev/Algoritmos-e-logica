bf974ecfd41c35c1dc9c41a63c2501211482cd93
beatrizmdev <beatrizbmedeirosdev@gmail.com>
2025-06-10 18:58:23 -0300

    feat: ternary operator

---
+++ linguagensComerciais/c#/operadorTernario/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace operadorTernario
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 3;
+
+            string resultado = (numero % 2 == 0) ? "par." : "ímpar."; // (condição é true?) ? se true : se false;
+
+            Console.WriteLine("O número " + numero + " é " + resultado);
+
+
+        }
+    }
+}
